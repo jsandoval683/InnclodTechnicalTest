f66a479963fada41ecba24a52bd3ec91190d93a7
Beto <jsandoval683@unab.edu.co>
2024-10-11 13:39:55 -0500

    interfaces and services created

--- src/app/projects/services/project-api.service.ts
+++ src/app/projects/services/project-api.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Project } from '../interfaces/project';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectApiService {
 
-  constructor() { }
+  apiPath: string = '/users';
+
+  http = inject(HttpClient);
+
+  getProjects = (): Observable<Project[]> => this.http.get<Project[]>(`${environment.basePath}${this.apiPath}`);
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    basePath: 'https://jsonplaceholder.typicode.com/',
+    userLogin: 'admin',
+    passwordLogin: 'admin',
+    fakeToken: 'DFAHFxgcfhDSFGhDFGJ2145df1ghDFGJ'
+};
